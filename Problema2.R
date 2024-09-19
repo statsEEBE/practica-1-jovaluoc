35014e363e1a423ab5c65c31206df34fd8c37c55
josep.valcarce.girbau <josep.valcarce.girbau@auleseebe.upc.edu>
2024-09-19 13:32:06 +0200

    jjaja

--- Problema2.R
+++ Problema2.R
@@ -1,1 +1,8 @@
 #Codigo para problema 2
+ls()
+rm(list=ls())
+
+data = mtcars
+hist(data$mpg)
+# si fas un pie es una representació de les freqüencies de observacio es necessari posar 
+pie(table(data$cyl))
